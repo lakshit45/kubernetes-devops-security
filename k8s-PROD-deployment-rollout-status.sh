be9df032695fb1a51f84b5a8403006711b011282
lakshit45 <106504922+lakshit45@users.noreply.github.com>
2022-06-01 15:05:53 +0530

    Update k8s-PROD-deployment-rollout-status.sh

--- k8s-PROD-deployment-rollout-status.sh
+++ k8s-PROD-deployment-rollout-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 60s
 
-if [[ $(kubectl -n prod rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
+if [[ $(kubectl -n name rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
 then     
 	echo "Deployment ${deploymentName} Rollout has Failed"
     kubectl -n prod rollout undo deploy ${deploymentName}
